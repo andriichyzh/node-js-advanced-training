d567b77ee2f7ce97d7e67c6957e3c8a25f821800
andriichyzh <dev.andriichyzh@gmail.com>
2016-02-11 02:55:22 +0200

    Added Events module

--- examples/test.js
+++ examples/test.js
@@ -1,1 +1,20 @@
 'use strict';
+
+
+const EventEmitter = require('events');
+const util = require('util');
+
+
+function MyEmitter() {
+    EventEmitter.call(this);
+}
+
+util.inherits(MyEmitter, EventEmitter);
+
+const myEmitter = new MyEmitter();
+
+myEmitter.on('event', function(a, b) {
+    console.log(a, b, this instanceof MyEmitter);
+});
+
+myEmitter.emit('event', 'a', 'b');
